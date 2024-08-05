ca025a3ec0eb556b171b79c5fa2a73a1a254715a
Yash Patel <yyachodavadiya@gmail.com>
2024-08-05 16:53:39 +0530

    file add

---
+++ java.js
@@ -0,0 +1,34 @@
+let a = 20
+let b = 60
+
+let sum = a-b
+
+
+
+document.getElementById("h1").innerHTML= sum
+
+
+
+var x = "yash"
+
+console.log(x)
+
+var x = "shivay"
+
+console.log(x)
+
+
+{
+    let a = "10"
+    console.log(a)
+
+    var x = "hgbiuygtg"
+   console.log(x)
+
+}
+   const l = 10
+
+a=30
+
+
+   console.log(a);
