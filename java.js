6131078e348c81e5bdf3d53b82a871eca164d7ea
Yash Patel <yyachodavadiya@gmail.com>
2024-08-06 15:28:48 +0530

    new file add

--- java.js
+++ java.js
@@ -1,34 +1,46 @@
-let a = 20
-let b = 60
+// let a = 20
+// let b = 60
 
-let sum = a-b
-
-
-
-document.getElementById("h1").innerHTML= sum
+// let sum = a-b
 
 
 
-var x = "yash"
-
-console.log(x)
-
-var x = "shivay"
-
-console.log(x)
+// document.getElementById("h1").innerHTML= sum
 
 
-{
-    let a = "10"
-    console.log(a)
 
-    var x = "hgbiuygtg"
-   console.log(x)
+// var x = "yash"
 
-}
-   const l = 10
+// console.log(x)
 
-a=30
+// var x = "shivay"
+
+// console.log(x)
 
 
-   console.log(a);
+// {
+//     let a = "10"
+//     console.log(a)
+
+//     var x = "hgbiuygtg"
+//    console.log(x)
+
+// }
+//    const l = 10
+
+// a=30
+
+
+//    console.log(a);
+
+
+   let a = 30
+   let b = 20
+
+   let c = a > b 
+
+   c ? console.log("a is grater than b") : console.log("b is a grater than a")
+
+   let name = "test"
+
+   console.log(typeof name)
